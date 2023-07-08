a208ef18021645f43a32504d32e715458e7f32b7
alok96 <dwivedialok799@gmail.com>
2023-07-08 10:35:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import {BrowserRouter as Router , Route, Routes} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar";
+import ExercisesList from "./components/exercises-list";
+import EditExercise from "./components/edit-exercise";
+import CreateExercise from "./components/create-exercise";
+import CreateUser from "./components/create-user";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <div className="container">
+      <Routes>
+      < Route path="/" exact Component={ExercisesList} />
+      < Route path="/edit/:id"  Component={EditExercise} />
+      < Route path="/create"  Component={CreateExercise} />
+      < Route path="/user"  Component={CreateUser} />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-light bg-light navbar-expand-lg">
+        <div>
+        <Link to="/" className="navbar-brand">
+          <img src="../favicon.png" style={{ width: "35px" }} alt="running-boy-logo"></img>
+        </Link>
+        <Link to="/" className="navbar-brand">
+          ExerciseTracker
+        </Link>
+        </div>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">
+                Exercises
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">
+                Create Exercise Log
+              </Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/user" className="nav-link">
+                Create User
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
